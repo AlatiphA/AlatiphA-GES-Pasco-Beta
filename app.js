34ca93ebb4336dc555c56bc238ede4b71fda2e3a
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-12 19:24:41 +0000

    Bump app version to 2.2.6

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ let fontSize =
    APP VERSION
    Change this on every release
 ========================= */
-const APP_VERSION = "2.2.5";
+const APP_VERSION = "2.2.6";
 
 const versionEl =
   document.getElementById(
