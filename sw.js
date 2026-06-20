a73847d6cb1c679f6862ebf492b85bc039bfbd9f
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-20 11:28:06 +0000

    Bump app version from v1.0.1 to v1.0.5

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v1.0.1";
+const APP_VERSION  = "v1.0.5";
 const CACHE_NAME   = `alatipha-ges-pasco-${APP_VERSION}`;
 
 /* ====================
